b6ade38d479d9a40f6fc0b1b9b345025402bcecc
i-binhpn <i-binhnp@nissho-vn.com>
2024-03-04 17:03:38 +0700

    fix

--- src/common/layout/LayoutMain.jsx
+++ src/common/layout/LayoutMain.jsx
@@ -1,37 +1,49 @@
 import React, { useState } from "react";
-import { DesktopOutlined, FileOutlined, LogoutOutlined, PieChartOutlined } from "@ant-design/icons";
+import { LogoutOutlined } from "@ant-design/icons";
 import { Layout, Menu } from "antd";
 import Header from "./Header";
 import logo from "../../assets/img/logo.avif";
 import "./Layout.css";
 import { Link, useNavigate } from "react-router-dom";
+import MenuItem from "antd/es/menu/MenuItem";
+import { data } from "autoprefixer";
 const { Content, Footer, Sider } = Layout;
-
-function getItem(label, key, icon, children) {
-  return {
-    key,
-    icon,
-    children,
-    label,
-  };
-}
-const items = [
-  getItem("Employee", 1, <PieChartOutlined />),
-  getItem("Attendance", 2, <DesktopOutlined />),
-  getItem("Overtime", 3, <FileOutlined />),
-  getItem("Leave", 4, <FileOutlined />),
+const page = [
+  {
+    path: "/employee/all",
+    title: "Employee",
+    layout: true,
+    page: 1,
+  },
+  {
+    path: "/attendance/all",
+    title: "All Attendance",
+    layout: true,
+    page: 2,
+  },
+  {
+    path: "/overtime/all",
+    title: "All Overtime",
+    layout: true,
+    page: 3,
+  },
+  {
+    path: "/leave/all",
+    title: "All Leave",
+    layout: true,
+    page: 4,
+  },
 ];
 
 const LayoutMain = ({ children }) => {
   const [collapsed, setCollapsed] = useState(false);
-  const [page, setPage] = useState(1);
+  const [pageType, setPageType] = useState(1);
 
   const navigate = useNavigate();
-  const handleChangePage = (data) => {
-    setPage(data.key);
-    navigate("/employee/all");
+  const changePageType = (data) => {
+    navigate(data.path);
+    setPageType(data.page);
   };
-
   return (
     <Layout
       style={{
@@ -43,22 +55,19 @@ const LayoutMain = ({ children }) => {
         <div>
           <img src={logo} alt="" width="100%" className="logo" />
         </div>
-        <Menu
-          theme="dark"
-          defaultSelectedKeys={["1"]}
-          mode="inline"
-          items={items}
-          onClick={(data) => {
-            handleChangePage(data);
-          }}
-        />
+
+        <Menu theme="dark" defaultSelectedKeys={["1"]} mode="inline">
+          {page.map((data) => {
+            return <MenuItem onClick={() => changePageType(data)}>{data.title}</MenuItem>;
+          })}
+        </Menu>
         <Link to="/login" className="text-white px-6 ">
           <LogoutOutlined />
           Log out
         </Link>
       </Sider>
       <Layout>
-        <Header page={page} />
+        <Header page={pageType} />
         <Content style={{}}>
           <div>{children}</div>
         </Content>
