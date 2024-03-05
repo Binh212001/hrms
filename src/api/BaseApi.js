8ea1816cc5b50697a9a0aab1e9f5fb0e3dccf395
i-binhpn <i-binhnp@nissho-vn.com>
2024-03-05 11:31:55 +0700

    axios

---
+++ src/api/BaseApi.js
@@ -0,0 +1,31 @@
+import axios from "axios";
+import queryString from "query-string";
+
+const baseApi = axios.create({
+  baseURL: "http://localhost:8080/",
+  timeout: "300",
+  headers: {
+    "Content-Type": "application/json",
+  },
+  params: (params) => queryString.stringify(params),
+});
+
+baseApi.interceptors.response.use(
+  (res) => {
+    return res;
+  },
+  (err) => {
+    return Promise.reject(err);
+  }
+);
+
+baseApi.interceptors.request.use(
+  (config) => {
+    return config;
+  },
+  (err) => {
+    return Promise.reject(err);
+  }
+);
+
+export default baseApi;
